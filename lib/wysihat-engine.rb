70716c895b817820a2e0cde26b4565e3a01bd06e
Brandon Calloway <brandon@scullytown.com>
2009-12-16 14:14:01 -0500

    fixed view to include file upload

--- lib/wysihat-engine.rb
+++ lib/wysihat-engine.rb
@@ -17,9 +17,7 @@ module ActionView
         options["cols"], options["rows"] = size.split("x") if size && size.respond_to?(:split) 
         
         if options['buttons'] == nil || options['buttons'] == :all
-          buttons = [:bold, :italic, :underline, :strikethrough, :justify_left, :justify_center, :justify_right, :insert_ordered_list, :insert_unordered_list, :undo, :redo, :link, :html, :image]
-        else
-          buttons = options['buttons']
+          buttons = [:bold, :italic, :underline, :strikethrough, :justify_left, :justify_center, :justify_right, :insert_ordered_list, :insert_unordered_list, :undo, :redo, :link, :html, :image, :file]
         end
         
         javascript = "Event.observe(window, 'load', function() {" <<
